816b4795930ed20bb330134e0a37e93713cd6244
samiulbd22 <97846282+samiulbd22@users.noreply.github.com>
2024-09-05 01:27:21 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,7 +80,7 @@ const members = {
     runningMonth    :"September-24",
     previousMonth   :"August-24", // new addDecember
     upComingMonth   :"October 1,2024",
-    runningMealRate :126,
+    runningMealRate :120,
 
     "01814843266":{
         name:"Istiaq Ahmad Udoy",
@@ -95,7 +95,7 @@ const members = {
         garage_cost     :0,
         running_payable :2700,
         running_paid    :4500,
-        running_meal    :4,
+        running_meal    :5,
         // method calling
         preMonth,
         runningDiningCost,
@@ -115,7 +115,7 @@ const members = {
         garage_cost     :0,
         running_payable :3021,
         running_paid    :500,
-        running_meal    :4,
+        running_meal    :5,
         // method calling
         preMonth,
         runningDiningCost,
@@ -135,7 +135,7 @@ const members = {
         garage_cost     :200,
         running_payable :3994,
         running_paid    :0,
-        running_meal    :4,
+        running_meal    :5,
         // method calling
         preMonth,
         runningDiningCost,
@@ -175,7 +175,7 @@ const members = {
         garage_cost     :0,
         running_payable :568,
         running_paid    :0,
-        running_meal    :7,
+        running_meal    :8,
         // method calling
         preMonth,
         runningDiningCost,
@@ -209,13 +209,13 @@ const members = {
         post_payable        :2682,
         previous_paid       :3500,
         previous_dining_cost:1425,
-        payment             :undefined,
+        payment             :{"05/09/24-Cash":5000},
         // running month
         stay_cost       :3000,
         garage_cost     :0,
         running_payable :4337,
-        running_paid    :0,
-        running_meal    :10,
+        running_paid    :5000,
+        running_meal    :12,
         // method calling
         preMonth,
         runningDiningCost,
